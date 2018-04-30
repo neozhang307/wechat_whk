e0fb1db026a54195be9b53f041bf25351030a5ec
Lingqi Zhang <zlqi@qq.com>
2018-04-30 12:03:41 +0800

    new collection method nearly finished

--- local.py
+++ local.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template, redirect
 
 from message import message
 from sqlite_helper import SQLiteHelper
@@ -23,20 +23,38 @@ def index():
 def showname(name):
     return "hello" + name
 
-@app.route('/homework/<date>')
+@app.route('/showhomework/<date>')
 def showhomework(date):
     hwker = HomeworkHelper(date)
     user_d, user_d_rev = get_userlist()
     homework = {user_d[uid]:hwker.get(uid) for uid in range(501,518)}
     return render_template("show_dict.html", result=homework)
 
-@app.route('/checkwork/<date>')
-def checkwork(date):
+@app.route('/homework/')
+def homeworkform():
+    return render_template('my-form.html')
+
+@app.route('/homework/',methods=['POST'])
+def homework():
+    text = request.form['text'] 
+    return redirect('/showhomework/'+text,code=302)
+
+@app.route('/showcheckresult/<date>')
+def showcheckresult(date):
     hwker = HomeworkHelper(date)
     user_d, user_d_rev = get_userlist()
     delayed = hwker.checkunsubmit(user_d.keys())
     name = [user_d[id] for id in delayed]
     return render_template("show_list.html", data=name)
+
+@app.route('/checkwork/')
+def checkworkform():
+    return render_template('my-form.html')
+
+@app.route('/checkwork/',methods=['POST'])
+def checkwork():
+    text = request.form['text'] 
+    return redirect('/showcheckresult/'+text,code=302)
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000, debug=True)
